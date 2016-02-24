4098555000c40b25e7190d5c8b370afdc6e606f3
Juan Pablo Villa Russell <pablo.villa@schibsted.com.mx>
2016-02-23 20:08:26 -0600

    Initial Commit

---
+++ SuperHeros/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SuperHeros
+//
+//  Created by Juan Pablo Villa on 2/23/16.
+//  Copyright © 2016 Segundamano.mx. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
